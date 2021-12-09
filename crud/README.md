ce7cb7c99c81f8632c6d1548a3da21f41ef0e62f
Ulisses Queiroz D. Silva <uqs.dsilva@gmail.com>
2021-12-08 21:56:26 -0300

    Iniciando projeto

---
+++ crud/README.md
@@ -0,0 +1,2 @@
+Essa é uam aplicação para estudo de Next.js
+Visando abordar os fundamentos mais importantes 
---
+++ crud/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  // Informa quais pastas usaram classes tailwind
+  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', '.src/components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
